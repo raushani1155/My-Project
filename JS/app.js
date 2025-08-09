1861f82ed5c5864771f025b4806f95c62b06ead5
raushani1155 <raushaniraj308@gmail.com>
2025-08-09 12:32:58 +0530

    first commit

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,27 @@
-console.log("Hello World")
-let pencilPrice = 10;
-let eraserPrice = 5;
-console.log("The total price is :",pencilPrice + eraserPrice,"Rupees.");
+let todo = [];
+
+ let req =  prompt("please enter your request");
+
+ while(true){
+      if(req == "quit") {
+        console.log("quitting app");
+        break;
+      }
+ 
+      if(req =="list"){
+           console.log("----------------");
+          for(task of todo){
+              console.log(task);
+          }
+          console.log("-----------------");
+      } else if(req =="add"){
+        let task =  prompt("please enter the tast you want to add");
+        todo.push(task);
+        console.log("task added");
+      } 
+
+       req = prompt("please enter your request");
+    }
+      
+     
+    
